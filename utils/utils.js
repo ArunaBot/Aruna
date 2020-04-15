fb6246821817d008ecde95b023917eea3547578a
Lobo Metalúrgico <lobometalurgico@allonsve.com>
2020-04-15 20:50:40 -0300

    Add proper copyright to complement AGPLv3

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,21 @@
+/*
+    This File is part of ArunaBot
+    Copyright (C) LoboMetalurgico 2019-2020
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 function need(level) {
     var xp = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 850, 900, 950, 
         1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350, 1400, 1450, 1500, 1550, 1600, 1650, 1700, 1750, 1800, 1850, 1900, 1950, 2000, 2050, 9999999999999999999]
