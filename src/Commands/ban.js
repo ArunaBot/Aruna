3cb3256f4d67f034a12f49e877a1a7849b0ad3d8
Lobo Metalúrgico <hugolino1000@gmail.com>
2020-07-03 20:42:17 -0300

    Better Ban

--- src/Commands/ban.js
+++ src/Commands/ban.js
@@ -20,9 +20,7 @@ const Discord = require("discord.js");
 
 exports.run = async (aruna, message, args) => {
   
-  let buser = message.guild.member(
-    message.mentions.users.first() || message.guild.members.get(args[0])
-  );
+  let buser = await aruna.fetchUser(message.mentions.users.first() || args[0]);
   
   const error1 = new Discord.RichEmbed()
     .setAuthor(`Oops, ${message.author.username}`, message.author.avatarURL)
@@ -44,17 +42,22 @@ exports.run = async (aruna, message, args) => {
   const error4 = new Discord.RichEmbed()
     .setAuthor(`Oops, ${message.author.username}`, message.author.avatarURL)
     .setFooter(`Algo deu errado, ${message.author.username}`)
-    .setDescription(`Você não pode banir ${buser.user.username} pois este é o(a) dono(a) do servidor!`)
+    .setDescription(`Você não pode banir ${buser.username} pois este é o(a) dono(a) do servidor!`)
     .setTimestamp();
   const error5 = new Discord.RichEmbed()
     .setAuthor(`Oops, ${message.author.username}`, message.author.avatarURL)
     .setFooter(`Algo deu errado, ${message.author.username}`)
-    .setDescription(`Você não pode banir este usuário pois seu cargo é igual ou inferior ao de ${buser.user.username}.`)
+    .setDescription(`Você não pode banir este usuário pois seu cargo é igual ou inferior ao de ${buser.username}.`)
     .setTimestamp();
   const error6 = new Discord.RichEmbed()
     .setAuthor(`Oops, ${message.author.username}`, message.author.avatarURL)
     .setFooter(`Algo deu errado, ${message.author.username}`)
-    .setDescription(`Eu não posso banir este usuário pois meu cargo é igual ou inferior ao de ${buser.user.username}`)
+    .setDescription(`Eu não posso banir este usuário pois meu cargo é igual ou inferior ao de ${buser.username}`)
+    .setTimestamp();
+  const error7 = new Discord.RichEmbed()
+    .setAuthor(`Oops, ${message.author.username}`, message.author.avatarURL)
+    .setFooter(`Algo deu errado, ${message.author.username}`)
+    .setDescription(`Infelizmente não sei informar qual o erro. Sinto muito ${message.author.username} :(`)
     .setTimestamp();
   
   if (!message.member.hasPermission("BAN_MEMBERS"))
@@ -64,19 +67,25 @@ exports.run = async (aruna, message, args) => {
   
   if (!buser) return message.channel.send(error3);
   
-  if(message.guild.owner.id == buser.user.id)
-    return message.channel.send(error4)
-  if(buser.highestRole.position >= message.guild.members.get(message.author.id).highestRole.position && message.guild.owner.id !== message.author.id)
-    return message.channel.send(error5);
-  if(buser.highestRole.position >= message.guild.members.get(aruna.user.id).highestRole.position)
-    return message.channel.send(error6);
+  const guildBuser = message.guild.member(
+    message.mentions.users.first() || message.guild.members.get(args[0])
+  );
+
+  if(guildBuser) {
+    if(message.guild.owner.id == buser.id)
+      return message.channel.send(error4)
+    if(guildBuser.highestRole.position >= message.guild.members.get(message.author.id).highestRole.position && message.guild.owner.id !== message.author.id)
+      return message.channel.send(error5);
+    if(guildBuser.highestRole.position >= message.guild.members.get(aruna.user.id).highestRole.position)
+      return message.channel.send(error6);
+  }
   
-  if (args.join(" ").slice(22)) {
-    var breason = `Punido por: ${message.author.username}`;
+  if (!args.join(" ").slice(19)) {
+    var reason = `Punido por: ${message.author.username}`;
   } else {
-    var breason =
-      `Punido por: ${message.author.username} Com o Motivo: ` +
-      args.join(" ").slice(22);
+    var reason =
+      `Punido por: ${message.author.username} com o Motivo: ` +
+      args.join(" ").slice(19);
   }
 
   let embed = new Discord.RichEmbed()
@@ -85,15 +94,19 @@ exports.run = async (aruna, message, args) => {
     .addField("Usuário Banido: ", `${buser} id ${buser.id}`, false)
     .addField("Banido por: ", `<@${message.author.id}>`, false)
     .addField("Data do Banimento: ", message.createdAt, false)
-    .addField("Motivo: ", `${breason}`, false)
+    .addField("Motivo: ", `${reason}`, false)
     .setTimestamp();
 
-  message.guild.member(buser).ban(breason);
-  message.channel.send(embed);
+    message.channel.send(embed).then(async msg => {
+      await message.guild.ban(buser, reason).catch(err => {
+          console.log(err)
+          msg.edit(error7);
+      })
+  })
 };
 
 exports.config = {
-  name: "ban",
-  aliases: ["banir"],
+  name: "banir",
+  aliases: ["ban"],
   category: `👮‍♂️ Moderação`
 };
