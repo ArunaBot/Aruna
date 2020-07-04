5e464fe3098c63d4c1aa927038afc3e583efbe4d
Lobo Metalúrgico <hugolino1000@gmail.com>
2020-07-04 01:48:06 -0300

    Minor Bug Fixed

--- .Configs/mongoose.js
+++ .Configs/mongoose.js
@@ -44,7 +44,7 @@ var Guild = new Schema({
   ticketEnable: { type: Boolean, default: false },
   autoRole: { type: Boolean, default: false },
   autoRoleRole: { type: String, default: null },
-  isPremiun: { type: Boolean, default: false },
+  isPremium: { type: Boolean, default: false },
   isPartner: { type: Boolean, default: false }
 });
 
