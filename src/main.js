83a038774b0bfb2cba6b9baa7b6a5c8383374bdf
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2020-08-10 13:08:29 -0300

    Removing old dependencies

--- src/main.js
+++ src/main.js
@@ -19,18 +19,6 @@
 
 require('events').EventEmitter.defaultMaxListeners = 999;
 
-const express = require('express');
-const http = require('http');
-const app = express();
-app.get('/', (request, response) => {
-  console.log(Date.now() + ' Ping Received');
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
-
 const Discord = require('discord.js');
 const fs = require('fs');
 const { config } = require('../Configs');
