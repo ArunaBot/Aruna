3af6d198e03f84a85f4159a9055fa16500b73ad2
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2020-08-10 17:37:22 -0300

    Mention Texts

--- src/Events/message.js
+++ src/Events/message.js
@@ -26,17 +26,17 @@ exports.run = async (aruna, message) => {
 
   if (message.channel.type == 'dm') {
     const dmUser = await database.Users.findOne({ _id: message.author.id });
-    const defaultDmIntL = require('../../languages/bot/br/internal.json');
+    const defaultDmEventL = require('../../languages/bot/br/internal.json');
     if (!dmUser) {
-      return message.reply(defaultDmIntL.dmError);
+      return message.reply(defaultDmEventL.message.errors.dmError);
     } else {
-      var dmIntL = '';
+      var dmEventL;
       if (dmUser.language == null) {
-        dmIntL = defaultDmIntL;
+        dmEventL = defaultDmEventL;
       } else {
-        dmIntL = require(`../../languages/bot/${dmUser.language}/internal.json`);
+        dmEventL = require(`../../languages/bot/${dmUser.language}/internal.json`);
       }
-      return message.reply(dmIntL.dmError);
+      return message.reply(dmEventL.message.errors.dmError);
     }
   }
 
@@ -80,10 +80,9 @@ exports.run = async (aruna, message) => {
       
   const lang = require(`../../languages/bot/${language}/events.json`);
   const langc = require(`../../languages/bot/${language}/commands.json`);
-  const intL = require(`../../languages/bot/${language}/internal.json`);
 
-  const emojiError = intL.errors.emojiError;
-  const linkError = intL.errors.linkError;
+  const emojiError = lang.message.errors.emojiError;
+  const linkError = lang.message.errors.linkError;
 
   const mention = [`<@${aruna.user.id}>`, `<@!${aruna.user.id}>`];
 
@@ -94,14 +93,14 @@ exports.run = async (aruna, message) => {
       return message.reply(linkError);
     } else if (message.content === mention) {
       const embed = new Discord.RichEmbed()
-        .setAuthor('Quem me chama?')
+        .setTitle(language.message.mention.title)
         .setDescription(
-          `Ah, olá ${message.author.username}, como está? Eu sou a ${aruna.user.username}.\n
-              Bom, caso queira saber minha função, ela é ajudar seu guild de várias formas criativas e diferentes!\n
-              Talvez você não saiba meu prefixo neste guild e por isso me chamou. Se essa for sua dúvida, ele é \`\`${prefix}\`\`.\n\n
-              Se você tiver alguma sugestão ou tiver encontrado algum parafuso meu perdido por aí, entre no meu [guild de suporte](${links.supportServers[0]}) para falar com meu criador.\n\n
-              Se quiser me adicionar em seu guild, basta clicar [aqui](${links.invites[0]})!\n\n
-              E por fim, obrigada por me chamar. Foi ótimo te explicar quem sou <3`
+          `${language.message.mention.line1.replace('[username]', message.author.username).replace('[me]', aruna.user.username)}\n
+              ${language.message.mention.line2}\n
+              ${language.message.mention.line3.replace('[prefix]', prefix)}\n\n
+              ${language.message.mention.line4.replace('[url]', links.supportServers[0])}\n\n
+              ${language.message.mention.line5.replace('[url]', links.invites[0])}\n\n
+              ${language.message.mention.line6}`
         )
         .setColor('#8400ff')
         .setTimestamp();
