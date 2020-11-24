baa5df3035de8630ae6d88109e9a9fdf5ad62b14
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2020-11-24 19:52:13 -0300

    Adding AGPL required text

--- src/sharding.js
+++ src/sharding.js
@@ -16,6 +16,7 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+
 const Discord = require('discord.js');
 const chalk = require('chalk');
 const { config } = require('../Configs');
