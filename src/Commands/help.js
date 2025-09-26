b46f0524d17b6c73d16a6a0b2fcc587373f1a18f
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2025-09-25 22:26:17 -0300

    chore: update copyright year

--- src/Commands/help.js
+++ src/Commands/help.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 /*
     This File is part of ArunaBot
-    Copyright (C) LoboMetalurgico (and contributors) 2019-2021
+    Copyright (C) LoboMetalurgico (and contributors) 2019-2025
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
